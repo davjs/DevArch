a9ff91a50dfe3e2852a98dbc061517d517916027
davidkron <david_jensen3@hotmail.com>
2015-10-17 18:05:27 +0200

    Added missing files

---
+++ Lib/ArchView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace DevArch.lib
+{
+    /// <summary>
+    /// Interaction logic for Diagram.xaml
+    /// </summary>
+    [ContentProperty("MainContent")]
+    public partial class ArchView : UserControl
+    {
+        public ArchView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lib/DevArch.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Presentation;
+using EnvDTE;
+using Logic;
+
+namespace DevArch.lib
+{
+    public static class DevArch
+    {
+        public static void RenderAllArchDiagramsToFiles(_DTE enivorment)
+        {
+            var solutionDir = Path.GetDirectoryName(enivorment.Solution.FullName);
+            var modelGen = new DiagramFromModelDefinitionGenerator(enivorment);
+            var modelDefs = modelGen.GetModelDefinitions();
+            foreach (var modelDef in modelDefs)
+            {
+                var tree = modelGen.GenerateDiagram(modelDef);
+                var outputPath = solutionDir + "\\" + modelDef.Output.Path;
+                if (File.Exists(outputPath))
+                    File.Delete(outputPath);
+                BitmapRenderer.RenderTreeToBitmap(tree, outputPath, modelDef.Output);
+            }
+        }
+
+        public static void RenderCompleteDiagramToView(_DTE enivorment,ref ArchView view)
+        {
+            var modelGen = new DiagramFromModelDefinitionGenerator(enivorment);
+            var tree = modelGen.GenerateDiagram(ModelDefinition.RootDefault);
+            var viewModel = LayerMapper.TreeModelToArchViewModel(tree);
+            view.Diagram.RenderModel(viewModel);
+        }
+    }
+}
