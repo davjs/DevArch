097928b630a1afc2074f2282a296ee9a39b32c9b
davidkron <david_jensen3@hotmail.com>
2016-01-03 18:35:58 +0100

    Revert "Removed old vsix project"
    This reverts commit 46f9cf5808304d368f0668ef9b811ee0db453dcc.

---
+++ Clients/VisualStudio/VsixContainer/ToolsMenu/Commands/CommandFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+
+namespace ToolsMenu.Commands
+{
+    class CommandFactory
+    {
+        private readonly OleMenuCommandService _commandService;
+
+        public CommandFactory(Package package)
+        {
+            _commandService = (package as IServiceProvider).GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (_commandService == null) throw new ServiceUnavailableException(typeof(IMenuCommandService));
+        }
+
+        // Adds our command handlers for menu (commands must exist in the command table file)
+        public void AddCommand(CommandBase command,CommandID commandId)
+        {
+            _commandService.AddCommand(new MenuCommand(command.OnClick,commandId));
+        }
+    }
+}
