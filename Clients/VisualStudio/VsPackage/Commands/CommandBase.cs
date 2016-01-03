59d8d072e8638e9a03b0ac2d2cc7e7d68781e93c
davidkron <david_jensen3@hotmail.com>
2016-01-03 19:55:14 +0100

    Restored lost files from the vsixproject

---
+++ Clients/VisualStudio/VsPackage/Commands/CommandBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ToolsMenu.Commands
+{
+    abstract class CommandBase
+    {
+        protected readonly IServiceProvider ServiceProvider;
+
+        protected CommandBase(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public abstract void OnClick(object sender, EventArgs e);
+    }
+}
